70c907b0df2bea79409039801062ecd521b7bbb9
최용훈 <cyh2607@gmail.com>
2024-04-25 17:37:55 +0900

    Test : testapp 구성, Documnet : readme작성

---
+++ android/app/TestApp/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.testapp.ui.theme.TestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Screen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen() {
+    val(clicks, setClicks) = remember { mutableIntStateOf(0) }
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)
+    ){
+        Text(
+            "Total clicks: $clicks"
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            setClicks(clicks+1)
+        }) {
+            Text(text = "Click me!"
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestAppTheme {
+        Screen()
+    }
+}
