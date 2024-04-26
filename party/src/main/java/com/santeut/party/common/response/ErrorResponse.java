566281b29667e17c930d7d04888fbd780379ff60
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-04-26 10:58:50 +0900

    Feat: 에러 핸들링 및 응답 객체 추가

---
+++ party/src/main/java/com/santeut/party/common/response/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.santeut.party.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String name;
+    private final String message;
+}
