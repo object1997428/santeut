4e82664e5b9a36c701fc39efbdd411d108043cb0
양윤모 <yym4481@naver.com>
2024-04-26 14:59:37 +0900

    Feat: Code 컨밴션을 위한 common 파일 추가
    Exception, custom ResponseEntity 등 추가

---
+++ community/src/main/java/com/santeut/community/common/HealthCheckController.java
@@ -0,0 +1,23 @@
+package com.santeut.community.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping(value = "/party/healthcheck")
+    public HealthStatus healthcheck() {
+        return new HealthStatus("ok");
+    }
+
+    class HealthStatus {
+        private String status;
+        HealthStatus(String status) {
+            this.status = status;
+        }
+        String getStatus() {
+            return status;
+        }
+    }
+
+}
