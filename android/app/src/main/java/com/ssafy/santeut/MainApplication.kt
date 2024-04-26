f1f3f136a7f01ab547ab4d2458e9fb0c1cbae6d5
yeongdae <oooo_0@naver.com>
2024-04-26 12:29:11 +0900

    Init: 프로젝트 생성 및 의존성 추가

---
+++ android/app/src/main/java/com/ssafy/santeut/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.ssafy.santeut
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
