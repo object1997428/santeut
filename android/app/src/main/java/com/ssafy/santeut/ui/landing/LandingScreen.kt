8bc8b14533743736665e6927eb01e193b98636d4
yeongdae <oooo_0@naver.com>
2024-04-29 08:52:09 +0900

    Feat: Landing Page 구현
    - Event / CSS

--- android/app/src/main/java/com/ssafy/santeut/ui/landing/LandingScreen.kt
+++ android/app/src/main/java/com/ssafy/santeut/ui/landing/LandingScreen.kt
@@ -1,15 +1,33 @@
 package com.ssafy.santeut.ui.landing
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.ssafy.santeut.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun LandingScreen(
@@ -17,24 +35,78 @@ fun LandingScreen(
     onNavigateHome: () -> Unit,
     viewModel: UserViewModel = hiltViewModel()
 ) {
+    val effect by viewModel.uiEvent.collectAsStateWithLifecycle()
 
-    val state = viewModel.state
+    Log.d("Landing... ", "")
     viewModel.checkAuth()
 
-    LaunchedEffect (state.token){
-        Log.d("Landing... ", state.token)
-        if(state.token.isBlank()){
-            // 토큰 검증이 아니라 자동 로그인이 더 알맞을 수도 있을 것 같음.
-            onNavigateLogin()
-        }else {
-            onNavigateHome()
+    LaunchedEffect(effect) {
+        if(effect is UserViewModel.AuthUiEvent.Auth){
+            delay(1000)
+            val authEvent = effect as UserViewModel.AuthUiEvent.Auth
+            if (authEvent.auth) {
+                onNavigateHome()
+            } else {
+                onNavigateLogin()
+            }
         }
     }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-    ){
-        Text(text = "로딩 중...")
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text = "산뜻",
+            textAlign = TextAlign.Center,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Landing() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text = "산뜻",
+            textAlign = TextAlign.Center,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+            )
+        }
     }
 }
