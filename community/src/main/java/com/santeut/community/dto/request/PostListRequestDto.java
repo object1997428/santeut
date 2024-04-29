ef53267b83113828276294b61951b506f00f2ce6
양윤모 <yym4481@naver.com>
2024-04-29 10:09:22 +0900

    Feat: PostListReqeustDto, PostListResponseDto 구현

---
+++ community/src/main/java/com/santeut/community/dto/request/PostListRequestDto.java
@@ -0,0 +1,15 @@
+package com.santeut.community.dto.request;
+
+import lombok.*;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostListRequestDto {
+    private Integer userId;
+    private String postTitle;
+    private String postContent;
+    private Character postType;
+    private Integer userPartyId;
+}
