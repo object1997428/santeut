6143398b5e62b317206eecd9b4ccb13ee8d4900a
kdw <hide4321@naver.com>
2024-04-30 10:47:53 +0900

    Fix: conflict

---
+++ auth/src/main/java/com/santeut/auth/dto/requestDto/SignUpRequestDto.java
@@ -0,0 +1,18 @@
+package com.santeut.auth.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequestDto {
+
+    private String userNickname;
+    private String userLoginId;
+    private String userPassword;
+    private String userBirth;
+    private String userProfile;
+    private String userGender;
+}
