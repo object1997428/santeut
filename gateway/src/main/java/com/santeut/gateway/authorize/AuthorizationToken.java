b150b0b90e23d66785b89433a2ae504d10b6b0da
kdw <hide4321@naver.com>
2024-04-30 10:51:06 +0900

    Feat: auth-dev branch gatewayFolder

--- gateway/src/main/java/com/santeut/gateway/authorize/AuthorizationToken.java
+++ gateway/src/main/java/com/santeut/gateway/authorize/AuthorizationToken.java
@@ -1,8 +1,6 @@
 package com.santeut.gateway.authorize;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -14,10 +12,10 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class AuthorizationToken {
 
-            @Autowired
-            private Environment env;
+            @Value("${jwt.secretKey}")
+            private String jwtSecret;
             public boolean validateToken(String token){
-                String jwtSecret = env.getProperty("jwt.secretKey");
+
                 try {
                     SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
                     Jwts.parser()
---
+++ gateway/src/test/java/com/santeut/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santeut.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
