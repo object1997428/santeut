ad1987a7b08a3f63d47dffb1ec75d8b2de2526fb
박나린 <xw2nana@gmail.com>
2024-05-04 22:17:28 +0900

    Feat: Tip 공유 게시판 UI

--- android/app/src/main/java/com/santeut/ui/community/ShareTipsScreen.kt
+++ android/app/src/main/java/com/santeut/ui/community/ShareTipsScreen.kt
@@ -2,22 +2,34 @@ package com.santeut.ui.community
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Comment
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ShareTipsScreen(){
+fun ShareTipsScreen() {
     LazyColumn(
         modifier = Modifier
-            .background(color = Color.Green)
+            .background(color = Color.White)
             .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
@@ -29,17 +41,40 @@ fun ShareTipsScreen(){
 
 @Composable
 fun TipDetail() {
-    Column {
-        Row {
-            Text(text = "Title ")
-            Text(text = "Writer")
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+        Text(text = "Title", style = MaterialTheme.typography.h6)
+        Text(text = "Content", style = MaterialTheme.typography.body1)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(vertical = 8.dp),
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Filled.Comment,
+                    contentDescription = "Comment",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(text = "4", style = MaterialTheme.typography.body2)
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = "Favorite",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(text = "4", style = MaterialTheme.typography.body2)
+            }
+            Text(text = "15분 전", style = MaterialTheme.typography.caption)
+            Text(text = "nickname", style = MaterialTheme.typography.caption)
         }
-        Text(text = "내용")
     }
 }
 
 @Preview
 @Composable
-fun ShareTipsPreview(){
+fun ShareTipsPreview() {
     ShareTipsScreen()
 }
