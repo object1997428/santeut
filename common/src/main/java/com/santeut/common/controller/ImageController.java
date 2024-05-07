43b3ba8b272940099dc73b154c146a855f61cfa0
양윤모 <yym4481@naver.com>
2024-05-07 12:37:58 +0900

    Feat: 게시글 이미지 불러오기 API 구현
    +) controller단에서 respository에 저장 했던 부분 service로 분리

--- common/src/main/java/com/santeut/common/controller/ImageController.java
+++ common/src/main/java/com/santeut/common/controller/ImageController.java
@@ -4,6 +4,7 @@ import com.santeut.common.common.response.BasicResponse;
 import com.santeut.common.common.util.ResponseUtil;
 import com.santeut.common.entity.ImageEntity;
 import com.santeut.common.repository.ImageRepository;
+import com.santeut.common.service.ImageService;
 import com.santeut.common.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,19 +21,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ImageController {
 
-    private final ImageRepository  imageRepository;
+    private final ImageService imageService;
 
     // 게시글 이미지 저장 (CREATE)
     @PostMapping("/upload/{referenceId}/{referenceType}")
     public ResponseEntity<BasicResponse> saveImageUrl(@PathVariable Integer referenceId, @PathVariable Character referenceType,@RequestBody Map<String, String> imageUrl) {
-
-        imageRepository.save(ImageEntity.builder()
-                .imageReferenceId(referenceId)
-                .imageReferenceType(referenceType)
-                .imageUrl(imageUrl.get("imageUrl"))
-                .build()
-        );
-
+        imageService.savePostImages(referenceId, referenceType, imageUrl.get("imageUrl"));
         return ResponseUtil.buildBasicResponse(HttpStatus.OK, "성공적으로 이미지가 저장되었습니다.");
+    }
+
+    // 게시글 이미지들 불러오기 (READ)
+    @GetMapping("/{referenceId}/{referenceType}")
+    public ResponseEntity<BasicResponse> getImages(@PathVariable Integer referenceId, @PathVariable Character referenceType) {
+        return ResponseUtil.buildBasicResponse(HttpStatus.OK, imageService.getPostImages(referenceId, referenceType));
     }
 }
