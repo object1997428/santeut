f8c843f6f931d0a1b0933b421a580302d76cfb2f
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-05-09 11:26:44 +0900

    Fix: Origin 모두 허용으로 변경

--- hiking/src/main/java/com/santeut/hiking/common/config/WebSocketConfig.java
+++ hiking/src/main/java/com/santeut/hiking/common/config/WebSocketConfig.java
@@ -12,8 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-//                .setAllowedOrigins("*")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("*")
+//                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
 
