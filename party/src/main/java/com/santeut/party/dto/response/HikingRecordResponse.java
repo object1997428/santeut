d46d5ecec4de210684b723730b2d3422f43db24f
김가영 <kkayoung0@gmail.com>
2024-05-09 14:39:36 +0900

    feat: 등산 기록 조회 api 추가

---
+++ party/src/main/java/com/santeut/party/dto/response/HikingRecordResponse.java
@@ -0,0 +1,44 @@
+package com.santeut.party.dto.response;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class HikingRecordResponse {
+
+  public int partyUserId;
+  public String partyName;
+  public String guildName;
+  public String mountainName;
+  public String schedule;  // yyyy-MM-dd
+  public Double distance;  // 이동거리(km)  cm -> km
+  public Integer height;   // 최고고도(m)
+  public Integer duration; // 이동시간(분)
+
+  public static HikingRecordResponse of(int partyUserId, String partyName, String guildName,
+      String mountainName, LocalDateTime schedule,
+      Integer distance, Integer height, Integer duration) {
+
+    Double distanceInKm = null;
+    if(distance != null) {
+      distanceInKm = Math.round(((distance * 1.0) / 100000)*10)/10.0;
+    }
+
+    return HikingRecordResponse.builder()
+        .partyUserId(partyUserId)
+        .partyName(partyName)
+        .guildName(guildName)
+        .mountainName(mountainName)
+        .schedule(schedule.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+        .distance(distanceInKm)
+        .height(height)
+        .duration(duration)
+        .build();
+  }
+
+}
