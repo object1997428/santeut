40bbd757e9f23eeb0d499c81e3cacd401c9f1de3
박나린 <xw2nana@gmail.com>
2024-05-10 09:03:29 +0900

    Feat: Eureka Client

--- hiking/src/main/java/com/santeut/hiking/HikingApplication.java
+++ hiking/src/main/java/com/santeut/hiking/HikingApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
 @EnableWebSocketMessageBroker
