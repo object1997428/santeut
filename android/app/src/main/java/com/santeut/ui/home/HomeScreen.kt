1c9da360cf47af83e9dc730f0a933b00ff5fc33d
박나린 <xw2nana@gmail.com>
2024-05-10 23:57:00 +0900

    Feat: 인기있는 산 -> 산 상세로 이동

--- android/app/src/main/java/com/santeut/ui/home/HomeScreen.kt
+++ android/app/src/main/java/com/santeut/ui/home/HomeScreen.kt
@@ -79,7 +79,7 @@ fun HomeScreen(
             )
         }
         item {
-            PopMountainCard(mountainViewModel)
+            PopMountainCard(mountainViewModel, navController)
         }
         item {
             MyGuildCard()
@@ -155,7 +155,8 @@ fun SearchMountainBar(
 
 @Composable
 fun PopMountainCard(
-    mountainViewModel: MountainViewModel
+    mountainViewModel: MountainViewModel,
+    navController: NavController
 ) {
 
     val mountains by mountainViewModel.mountains.observeAsState(emptyList())
@@ -179,18 +180,19 @@ fun PopMountainCard(
                 .fillMaxWidth(),
         ) {
             items(mountains) { mountain ->
-                MountainCard(mountain)
+                MountainCard(mountain, navController)
             }
         }
     }
 }
 
 @Composable
-fun MountainCard(mountain: MountainResponse) {
+fun MountainCard(mountain: MountainResponse, navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxHeight()
-            .width(160.dp),
+            .width(160.dp)
+            .clickable(onClick = { navController.navigate("mountain/${mountain.mountainId}") }),
     ) {
         Column(
             modifier = Modifier
