5693f40451702039a4c0c79a66c0c1957ca592cf
박나린 <xw2nana@gmail.com>
2024-05-10 09:00:02 +0900

    Feat: Eureka Client

--- common/src/main/java/com/santeut/common/CommonApplication.java
+++ common/src/main/java/com/santeut/common/CommonApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommonApplication {
 
     public static void main(String[] args) {
