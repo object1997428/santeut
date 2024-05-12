d9d763756eb8663168bd58d438db13067d059926
주혜련 <object1997@naver.com>
2024-05-13 06:45:12 +0900

    Fix: 잘못올린 서버 파일 삭제

--- guild/src/main/java/com/santeut/guild/common/exception/AccessDeniedException.java
+++
@@ -1,8 +0,0 @@
-package com.santeut.guild.common.exception;
-
-//필요한 에러 이렇게 만들어서 쓰세요
-public class AccessDeniedException extends RuntimeException{
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
