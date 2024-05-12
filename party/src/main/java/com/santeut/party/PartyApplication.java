ce5fd26e3b394c4210b7983a790f0f51004fa239
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-05-13 06:25:37 +0900

    Feat: 랭킹 갱신 추가

--- party/src/main/java/com/santeut/party/PartyApplication.java
+++ party/src/main/java/com/santeut/party/PartyApplication.java
@@ -2,6 +2,7 @@ package com.santeut.party;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableCaching
 public class PartyApplication {
 
     public static void main(String[] args) {
