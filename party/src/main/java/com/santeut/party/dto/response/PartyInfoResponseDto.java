17ec8395979eb1b16a67228d195b56c3649277ee
김가영 <kkayoung0@gmail.com>
2024-05-13 11:43:10 +0900

    fix: 소모임 조회 페이지네이션 제거

--- party/src/main/java/com/santeut/party/dto/response/PartyInfoResponseDto.java
+++ party/src/main/java/com/santeut/party/dto/response/PartyInfoResponseDto.java
@@ -3,6 +3,7 @@ package com.santeut.party.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.santeut.party.entity.Party;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,33 +16,41 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL) // Null 값인 필드 제외
 public class PartyInfoResponseDto {
 
-  public int partyId;
-  public Integer partyUserId;
-  public String partyName;
-  public String guildName;
-  public String schedule;
-  public String mountainName;
-  public String place;
-  public int maxPeople;
-  public int curPeople;
-  public String owner;
-  public char status;
-  public Boolean isMember;
+  public List<PartyInfo> partyList;
 
-  public static PartyInfoResponseDto of(String nickname, Integer partyUserId, Party entity, Boolean isMember, String guildName) {
-    return PartyInfoResponseDto.builder()
-        .partyId(entity.getPartyId())
-        .partyUserId(partyUserId)
-        .partyName(entity.getPartyName())
-        .guildName(guildName)
-        .schedule(entity.getSchedule().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
-        .mountainName(entity.getMountainName())
-        .place(entity.getPlace())
-        .maxPeople(entity.getMaxParticipants())
-        .curPeople(entity.getParticipants())
-        .owner(nickname)
-        .status(entity.getStatus())
-        .isMember(isMember)
-        .build();
+  @Data
+  @Builder
+  public static class PartyInfo {
+
+    public int partyId;
+    public Integer partyUserId;
+    public String partyName;
+    public String guildName;
+    public String schedule;
+    public String mountainName;
+    public String place;
+    public int maxPeople;
+    public int curPeople;
+    public String owner;
+    public char status;
+    public Boolean isMember;
+
+    public static PartyInfo of(String nickname, Integer partyUserId, Party entity,
+        Boolean isMember, String guildName) {
+      return PartyInfo.builder()
+          .partyId(entity.getPartyId())
+          .partyUserId(partyUserId)
+          .partyName(entity.getPartyName())
+          .guildName(guildName)
+          .schedule(entity.getSchedule().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
+          .mountainName(entity.getMountainName())
+          .place(entity.getPlace())
+          .maxPeople(entity.getMaxParticipants())
+          .curPeople(entity.getParticipants())
+          .owner(nickname)
+          .status(entity.getStatus())
+          .isMember(isMember)
+          .build();
+    }
   }
 }
--- party/src/main/java/com/santeut/party/repository/PartyRepositoryImpl.java
+++ party/src/main/java/com/santeut/party/repository/PartyRepositoryImpl.java
@@ -1,12 +1,7 @@
 package com.santeut.party.repository;
 
-
-import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.DateTemplate;
-import com.querydsl.core.types.dsl.DateTimeTemplate;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.santeut.party.dto.response.PartyWithPartyUserIdResponse;
 import com.santeut.party.dto.response.QPartyWithPartyUserIdResponse;
@@ -15,14 +10,9 @@ import com.santeut.party.entity.QPartyUser;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 import com.santeut.party.entity.QParty;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -34,56 +24,37 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
   QPartyUser partyUser;
 
   @Override
-  public Page<Party> findPartyWithSearchConditions(int userId, Integer guildId, String name,
+  public List<Party> findPartyWithSearchConditions(int userId, Integer guildId, String name,
       LocalDate startDate,
-      LocalDate endDate, Pageable pageable) {
+      LocalDate endDate) {
     party = QParty.party;
 
-    List<Party> content = jpaQueryFactory
+    return jpaQueryFactory
         .selectFrom(party)
         .where(dateBetween(startDate, endDate), guildIdEq(guildId), partyNameContains(name),
             party.status.eq('B'))
-        .offset(pageable.getOffset())
-        .limit(pageable.getPageSize())
         .fetch();
-
-    JPAQuery<Long> count = jpaQueryFactory
-        .select(party.count())
-        .from(party)
-        .where(dateBetween(startDate, endDate), guildIdEq(guildId), partyNameContains(name),
-            party.status.eq('B'));
-
-    return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
   }
 
   @Override
-  public Page<PartyWithPartyUserIdResponse> findMyPartyWithSearchCondition(boolean includeEnd, LocalDate date, int userId,
-      Pageable pageable) {
+  public List<PartyWithPartyUserIdResponse> findMyPartyWithSearchCondition(boolean includeEnd,
+      LocalDate date, int userId
+  ) {
     party = QParty.party;
     partyUser = QPartyUser.partyUser;
 
-    List<PartyWithPartyUserIdResponse> content = jpaQueryFactory
+    return jpaQueryFactory
         .select(new QPartyWithPartyUserIdResponse(party, partyUser.partyUserId))
         .from(party)
         .join(partyUser)
         .on(partyUser.partyId.eq(party.partyId))
         .where(partyUser.userId.eq(userId), dateBetween(date, date), partyStatus(includeEnd))
         .fetch();
-
-    JPAQuery<Long> count = jpaQueryFactory
-        .select(party.count())
-        .from(party)
-        .join(partyUser)
-        .on(partyUser.partyId.eq(party.partyId)
-            .and(partyUser.userId.eq(userId)))
-        .where(dateBetween(date, date), partyStatus(includeEnd));
-
-    return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
   }
 
   private BooleanExpression partyStatus(boolean includeEnd) {
-    if(includeEnd) {
-      return party.status.in(List.of('B', 'P','E'));
+    if (includeEnd) {
+      return party.status.in(List.of('B', 'P', 'E'));
     } else {
       return party.status.in(List.of('B', 'P'));
     }
@@ -95,8 +66,10 @@ public class PartyRepositoryImpl implements PartyRepositoryCustom {
       return null;
     }
 
-    BooleanExpression isGoeStartDate = party.schedule.goe(LocalDateTime.of(dateFrom, LocalTime.MIN));
-    BooleanExpression isLoeEndDate = party.schedule.loe(LocalDateTime.of(dateTo, LocalTime.MAX).withNano(0));
+    BooleanExpression isGoeStartDate = party.schedule.goe(
+        LocalDateTime.of(dateFrom, LocalTime.MIN));
+    BooleanExpression isLoeEndDate = party.schedule.loe(
+        LocalDateTime.of(dateTo, LocalTime.MAX).withNano(0));
 
     return Expressions.allOf(isGoeStartDate, isLoeEndDate);
   }
