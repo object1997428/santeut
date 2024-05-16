bdb1d169a6f8f7d3e0ddc1e3e4c9e5fd2e5a60ea
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-05-17 02:33:51 +0900

    Style: 등산 일정 페이지

--- android/app/src/main/java/com/santeut/ui/mypage/MyScheduleScreen.kt
+++ android/app/src/main/java/com/santeut/ui/mypage/MyScheduleScreen.kt
@@ -1,9 +1,9 @@
 package com.santeut.ui.mypage
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -17,83 +17,282 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import com.santeut.ui.party.PartyViewModel
 import java.time.LocalDate
 import java.time.YearMonth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Event
+import androidx.compose.material.icons.filled.EventAvailable
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.santeut.R
 
-@OptIn(ExperimentalPagerApi::class)
+data class CalendarDate(
+    val day: Int,
+    val month: Int,
+    val year: Int
+)
+
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun MyScheduleScreen(partyViewModel: PartyViewModel = hiltViewModel()) {
+fun MyScheduleScreen(
+    partyViewModel: PartyViewModel = hiltViewModel(),
+) {
+    //등산기록 목록
     val myScheduleList by partyViewModel.myScheduleList.observeAsState(emptyList())
     val activeDates = myScheduleList.map { LocalDate.parse(it) }
 
-    val year = 2024
-    val month = 5
+    //나타내는 년, 월
+    var currentYear by remember { mutableStateOf(LocalDate.now().year) }
+    var currentMonth by remember { mutableStateOf(LocalDate.now().monthValue) }
+    val weekList = listOf("Su", "Mo", "Tu", "We", "Th", "Fr", "Sa")
 
-    LaunchedEffect (key1 = year, key2 = month){
-        partyViewModel.getMyScheduleList(year, month)
+    LaunchedEffect(key1 = currentYear, key2 = currentMonth) {
+        partyViewModel.getMyScheduleList(currentYear, currentMonth)
     }
 
-    val pagerState = rememberPagerState()
-
-    HorizontalPager(
-        count = 12, // 예: 현재 연도의 12개월
-        state = pagerState,
-        modifier = Modifier.fillMaxSize()
-    ) { pageIndex ->
-        val yearMonth = YearMonth.now().withMonth(pageIndex + 1)
-        MonthView(yearMonth, activeDates)
-    }
-}
-
-@Composable
-fun MonthView(yearMonth: YearMonth, activeDates: List<LocalDate>) {
-    val daysInMonth = remember(yearMonth) {
-        List(yearMonth.lengthOfMonth()) { day ->
-            yearMonth.atDay(day + 1)
+    fun daysInMonth(month: Int, year: Int): Int {
+        return when (month) {
+            2 -> if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) 29 else 28
+            4, 6, 9, 11 -> 30
+            else -> 31
         }
     }
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(7),
-        modifier = Modifier
-            .padding(16.dp)
-            .background(Color.White),
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(daysInMonth) { date ->
-            DateCell(date, activeDates.contains(date))
-        }
-    }
-}
+    //현재 달의 첫 날짜 예시: 6월 -> 6월 1일
+    val firstDayOfMonth = LocalDate.of(currentYear, currentMonth, 1)
+    //6월 1일의 요일 받아옴 -> dayOfWeek=3이면 6월 1일은 수요일
+    val dayOfWeek = firstDayOfMonth.dayOfWeek.value
+    //6월의 총 날짜수
+    val daysInCurrentMonth = daysInMonth(currentMonth, currentYear)
 
-@Composable
-fun DateCell(date: LocalDate, hasEvent: Boolean) {
-    Box(
-        modifier = Modifier
-            .size(48.dp)
-            .padding(4.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = date.dayOfMonth.toString())
-        if (hasEvent) {
-            Icon(
-                imageVector = Icons.Filled.Circle,
-                contentDescription = "Event marker",
-                tint = Color.Green,
+    Log.d("firstDayOfMonth", "MyScheduleScreen: $firstDayOfMonth")
+    Log.d("dayOfWeek", "MyScheduleScreen: $dayOfWeek")
+    Log.d("daysInCurrentMonth", "MyScheduleScreen: $daysInCurrentMonth")
+
+    val dates: List<CalendarDate> = (1..daysInCurrentMonth).map { day ->
+        CalendarDate(day, currentMonth, currentYear)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.padding(
+            horizontal = 35.dp,
+            vertical = 15.dp)) {
+            Column(
                 modifier = Modifier
-                    .size(16.dp)
-                    .align(Alignment.BottomEnd)
-            )
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFFE1E1E1),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(
+                        top = 4.dp,
+                        start = 6.dp,
+                        end=6.dp
+                    )
+            ) {
+                // < 2024년 5월 >
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            start = 16.dp,
+                            end = 16.dp,
+                            top = 8.dp
+                        )
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Icon(
+                        Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                        contentDescription = "keyboardArrowLeft",
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .clickable {
+                                Log.i("keyboardArrowLeft", "MyScheduleScreen: 달 내려감")
+                                if (currentMonth - 1 < 1) {
+                                    currentYear--
+                                    currentMonth = 12
+                                } else currentMonth--
+                            }
+                    )
+                    Text(
+                        text = "${currentYear}년 ${currentMonth}월",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            lineHeight = 32.sp,
+                        ),
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                    Icon(
+                        Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                        contentDescription = "keyboardArrowRight",
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .clickable {
+                                Log.i("keyboardArrowRight", "MyScheduleScreen: 달 올라감")
+                                if (currentMonth + 1 > 12) {
+                                    currentYear++
+                                    currentMonth = 1
+                                } else currentMonth++
+                            }
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(24.dp))
+                        .background(Color.White)
+                ) {
+                    // Su Mo Tu We Th Fr Sa
+                    FlowRow(
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp,
+                                top = 12.dp
+                            )
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        weekList.forEach { day ->
+                            Text(
+                                text = day,
+                                style = TextStyle(
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 17.sp,
+                                    lineHeight = 22.sp,
+                                ),
+//                            color = Color(0xff888888),
+                                modifier = Modifier.weight(1f),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    // DateItem
+                    FlowRow(
+                        modifier = Modifier
+                            .padding(
+                                start = 16.dp,
+                                end = 16.dp,
+                                bottom = 12.dp,
+                                top = 10.dp
+                            )
+                            .fillMaxWidth(),
+//                        horizontalArrangement = Arrangement.Center,
+                        maxItemsInEachRow = 7,
+                    ) {
+                        var startOfMonth = 0
+                        var select by remember { mutableStateOf("") }
+                        dates.forEach { date ->
+                            if (dayOfWeek > startOfMonth) {
+                                for (i in 1..dayOfWeek) {
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    startOfMonth++
+                                }
+                            }
+
+                            Column(
+                                modifier = Modifier
+                                    .padding(top = 10.dp)
+                                    .weight(1f)
+                                    .clickable {
+                                        select = "${date.year}${date.month}${date.day}"
+                                    },
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                startOfMonth++
+                                val dateLocal = LocalDate.of(date.year, date.month, date.day)
+
+                                Box(
+//                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier
+//                                        .height(20.dp) // 모든 Box에 고정 높이 지정
+//                                        .fillMaxWidth()
+//                                        .padding(4.dp) // 날짜별 간격 조정
+                                        .clip(RoundedCornerShape(40)) // 배경을 원형으로 클립
+                                        .background(
+                                            if (LocalDate
+                                                    .now()
+                                                    .isEqual(
+                                                        LocalDate.of(
+                                                            date.year,
+                                                            date.month,
+                                                            date.day
+                                                        )
+                                                    )
+                                            )
+                                                Color(0xFFE5DD90) // 오늘 날짜인 경우 배경색 변경
+                                            else
+                                                Color.Transparent, // 다른 날짜는 배경 투명 처리
+                                        )
+                                ) {
+                                    Text(
+                                        text = (date.day).toString(),
+                                        style = TextStyle(
+                                            fontWeight = FontWeight.Medium,
+                                            fontSize = 17.sp,
+                                            lineHeight = 22.sp,
+                                        ),
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center,
+                                        color = if (
+                                            LocalDate.now().dayOfMonth == date.day &&
+                                            LocalDate.now().monthValue == date.month &&
+                                            LocalDate.now().year == date.year
+                                        ) Color.White else Color.Black,
+                                    )
+                                }
+                                if (activeDates.contains(dateLocal)) {
+                                    Icon(
+                                        Icons.Filled.Circle,
+                                        contentDescription = "Active Event",
+                                        modifier = Modifier.size(20.dp),
+                                        tint = Color(0xFF678C40)
+                                    )
+                                }
+                                else {
+                                    Spacer(Modifier.size(20.dp)) // 아이콘이 없을 경우 같은 크기의 공간을 확보합니다.
+                                }
+                            }
+                            if (startOfMonth == daysInCurrentMonth + dayOfWeek) {
+                                for (i in 1..7) Spacer(modifier = Modifier.weight(1f))
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
