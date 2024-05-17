2f58589327eb1b8bcdcea166e253f725af6cf769
김가영 <kkayoung0@gmail.com>
2024-05-18 03:16:19 +0900

    fix: import문 추가

--- android/app/src/main/java/com/santeut/ui/guild/GuildInfoScreen.kt
+++ android/app/src/main/java/com/santeut/ui/guild/GuildInfoScreen.kt
@@ -7,9 +7,11 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
