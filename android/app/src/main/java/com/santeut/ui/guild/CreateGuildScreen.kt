263eaf773f2f7897582848fbc5a3d07d411d5f85
김가영 <kkayoung0@gmail.com>
2024-05-17 23:16:08 +0900

    design: 동호회 생성 ui 수정

--- android/app/src/main/java/com/santeut/ui/guild/CreateGuildScreen.kt
+++ android/app/src/main/java/com/santeut/ui/guild/CreateGuildScreen.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import android.util.Log
 import android.webkit.MimeTypeMap
+import android.widget.Space
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -17,12 +18,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
@@ -41,21 +47,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.naver.maps.map.compose.Align
 import com.santeut.R
 import com.santeut.data.model.request.CreateGuildRequest
+import com.santeut.designsystem.theme.Green
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -92,142 +104,186 @@ fun CreateGuildScreen(
 
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .size(100.dp)
-                .padding(bottom = 16.dp)
-                .clickable {
-                    imagePickerLauncher.launch("image/*")
-                }
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            // 사진 선택
             val image: Painter = painterResource(id = R.drawable.logo)
+
+            Spacer(modifier = Modifier.height(8.dp))
             Image(
                 painter = image,
-                contentDescription = null
+                contentDescription = "동호회 사진",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Green, CircleShape)
+                    .clickable {
+                        imagePickerLauncher.launch("image/*")
+                    }
             )
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "이미지 삭제",
-                modifier = Modifier.align(Alignment.BottomCenter),
                 fontSize = 12.sp,
                 color = Color.Gray
             )
-        }
+            // 사진 선택 끝
 
-        OutlinedTextField(
-            value = guildName,
-            onValueChange = { guildName = it },
-            label = { Text("동호회 이름") },
-            placeholder = { Text("이름을 입력해주세요(최대 15자)") },
-            modifier = Modifier.fillMaxWidth()
-        )
+            // 동호회 이름, 설명 입력
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = guildName,
+                onValueChange = { guildName = it },
+                label = { Text("동호회 이름") },
+                placeholder = { Text("이름을 입력해주세요(최대 15자)") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = guildInfo,
-            onValueChange = { guildInfo = it },
-            label = { Text("동호회 설명") },
-            placeholder = { Text("설명을 입력해주세요") },
-            modifier = Modifier.fillMaxWidth()
-        )
+            OutlinedTextField(
+                value = guildInfo,
+                onValueChange = { guildInfo = it },
+                label = { Text("동호회 설명") },
+                placeholder = { Text("설명을 입력해주세요") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            // 동호회 이름, 설명 입력 끝
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            Text("공개여부")
-            Row {
-                RadioButton(selected = guildIsPrivate, onClick = { guildIsPrivate = false })
-                Text("공개")
-                Spacer(modifier = Modifier.width(8.dp))
-                RadioButton(selected = !guildIsPrivate, onClick = { guildIsPrivate = true })
-                Text("비공개")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            Text("성별")
-            Row {
-                RadioButton(selected = guildGender == 'A', onClick = { guildGender = 'A' })
-                Text("성별무관")
-                Spacer(modifier = Modifier.width(8.dp))
-                RadioButton(selected = guildGender == 'M', onClick = { guildGender = 'M' })
-                Text("남자")
-                Spacer(modifier = Modifier.width(8.dp))
-                RadioButton(selected = guildGender == 'F', onClick = { guildGender = 'F' })
-                Text("여자")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(
-                    BorderStroke(
-                        1.dp,
-                        MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
-                    ), shape = MaterialTheme.shapes.medium
+            // 동호회 공개 여부
+            Text(text ="공개여부",
+                modifier = Modifier.align(Alignment.Start)
                 )
-                .clickable {
-                    isDropdownExpanded = true
-                }
-                .padding(16.dp)
-        ) {
-            Text(selectedRegion, modifier = Modifier.align(Alignment.CenterStart))
-            DropdownMenu(
-                expanded = isDropdownExpanded,
-                onDismissRequest = { isDropdownExpanded = false }
+//            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
-                regions.forEachIndexed { index, region ->
-                    DropdownMenuItem(
-                        text = { Text(text = region) },
-                        onClick = {
-                            selectedRegion = region
-                            regionId = index
-                            isDropdownExpanded = false
-                        }
-                    )
+
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    ){
+                    RadioButton(selected = guildIsPrivate, onClick = { guildIsPrivate = false })
+                    Text("공개")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RadioButton(selected = !guildIsPrivate, onClick = { guildIsPrivate = true })
+                    Text("비공개")
                 }
             }
-        }
+            // 공개여부 끝
 
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            OutlinedTextField(
-                value = guildMinAge,
-                onValueChange = { guildMinAge = it },
-                label = { Text("최소 연령") },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    autoCorrect = true
-                ),
-                modifier = Modifier.weight(1f)
+            // 성별
+            Text(text ="성별",
+                modifier = Modifier.align(Alignment.Start)
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text("~")
-            Spacer(modifier = Modifier.width(8.dp))
-            OutlinedTextField(
-                value = guildMaxAge,
-                onValueChange = { guildMaxAge = it },
-                label = { Text("최대 연령") },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    autoCorrect = true
-                ),
-                modifier = Modifier.weight(1f)
-            )
-        }
+//            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    RadioButton(selected = guildGender == 'A', onClick = { guildGender = 'A' })
+                    Text("성별무관")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RadioButton(selected = guildGender == 'M', onClick = { guildGender = 'M' })
+                    Text("남자")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    RadioButton(selected = guildGender == 'F', onClick = { guildGender = 'F' })
+                    Text("여자")
+                }
+            }
+            // 성별 끝
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // 지역
+            Text(text ="지역",
+                modifier = Modifier.align(Alignment.Start)
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(
+                        BorderStroke(
+                            1.dp,
+                            MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                        ), shape = MaterialTheme.shapes.medium
+                    )
+                    .clickable {
+                        isDropdownExpanded = true
+                    }
+                    .padding(16.dp)
+            ) {
+                Text(selectedRegion, modifier = Modifier.align(Alignment.CenterStart))
+                DropdownMenu(
+                    expanded = isDropdownExpanded,
+                    onDismissRequest = { isDropdownExpanded = false }
+                ) {
+                    regions.forEachIndexed { index, region ->
+                        DropdownMenuItem(
+                            text = { Text(text = region) },
+                            onClick = {
+                                selectedRegion = region
+                                regionId = index
+                                isDropdownExpanded = false
+                            }
+                        )
+                    }
+                }
+            }
+            // 지역 끝
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // 연령
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                OutlinedTextField(
+                    value = guildMinAge,
+                    onValueChange = { guildMinAge = it },
+                    label = { Text("최소 연령") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        autoCorrect = true
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text("~")
+                Spacer(modifier = Modifier.width(8.dp))
+                OutlinedTextField(
+                    value = guildMaxAge,
+                    onValueChange = { guildMaxAge = it },
+                    label = { Text("최대 연령") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        autoCorrect = true
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            // 연령 끝
+
+        }
 
         Button(
             onClick = {
@@ -244,11 +300,18 @@ fun CreateGuildScreen(
                     )
                 )
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Green,
+                contentColor = Color.White,
+                disabledContainerColor = Color.LightGray,
+                disabledContentColor = Color.Black,
+            ),
         ) {
             Text("완료")
         }
     }
+
 }
 
 private fun getFileNameWithExtension(context: Context, uri: Uri): String {
