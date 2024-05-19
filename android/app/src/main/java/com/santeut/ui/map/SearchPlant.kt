3191b3bebb41ab41ea5679c0eadd2d4d29ccb41b
kdw <hide4321@naver.com>
2024-05-20 02:10:42 +0900

    Fix: 사진 촬영 버튼

--- android/app/src/main/java/com/santeut/ui/map/SearchPlant.kt
+++ android/app/src/main/java/com/santeut/ui/map/SearchPlant.kt
@@ -4,6 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.os.Environment
+import android.os.Handler
+import android.os.Looper
 import android.util.Base64
 import android.util.Log
 import android.widget.Toast
@@ -18,6 +20,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
@@ -27,11 +31,13 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -50,6 +56,7 @@ import com.ujizin.camposer.state.ImageCaptureResult
 import com.ujizin.camposer.state.rememberCameraState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.File
 
@@ -74,39 +81,63 @@ fun SearchPlant(plantViewModel: PlantViewModel = hiltViewModel()) {
         val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
         return rotatedBitmap.asImageBitmap()
     }
- 
-    CameraPreview(
-        cameraState = cameraState,
-    ) {
-        Button(onClick = {
 
-            coroutineScope.launch {
-                if (sheetState.isVisible) {
-                    sheetState.hide()
-                } else {
-                    sheetState.show()
-                }
-            }
-
-            val file = File(directory, fileName)
-            cameraState.takePicture(file) { result ->
-                if (result is ImageCaptureResult.Success) {
-                    imageFile.value = file
-
-                    CoroutineScope(Dispatchers.IO).launch {
-                        plantViewModel.identifyPlant(file)
-                        isButton.value = true;
-//                                sheetState.show()
-                    }
-                } else {
-                    Toast.makeText(context, "사진 촬영에 실패했습니다", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Icon(Icons.Rounded.Add, contentDescription = "사진 촬영")
-        }
+            CameraPreview(
+                cameraState = cameraState,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        if (sheetState.isVisible) {
+                            sheetState.hide()
+                        } else {
+                            sheetState.show()
+                        }
+                    }
+
+                    val file = File(directory, fileName)
+                    cameraState.takePicture(file) { result ->
+                        if (result is ImageCaptureResult.Success) {
+                            imageFile.value = file
+
+                            coroutineScope.launch(Dispatchers.IO) {
+                                plantViewModel.identifyPlant(file)
+                                isButton.value = true
+                            }
+                        } else {
+                            Toast.makeText(context, "사진 촬영에 실패했습니다", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 32.dp)
+                    .size(72.dp),
+                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White) // 버튼 색상
+
+            ) {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = "사진 촬영",
+                    modifier = Modifier.size(36.dp),
+                    tint = Color.Black
+                )
+            }
     }
+
+    // 시연
+//    if (isButton.value){
+//      LaunchedEffect(Unit) {
+//        delay(5000L)
+//        plantViewModel.isLoading.value = false
+//      }
+//    }
 
     if(plantViewModel.isLoading.value && isButton.value) {
         Box(
@@ -128,7 +159,6 @@ fun SearchPlant(plantViewModel: PlantViewModel = hiltViewModel()) {
                 )
             }
         }
-
     }
 
     if (!plantViewModel.isLoading.value) {
@@ -147,12 +177,13 @@ fun SearchPlant(plantViewModel: PlantViewModel = hiltViewModel()) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .fillMaxHeight()
-//                        .height(400.dp)
                         .clip(RoundedCornerShape(8.dp)),
                     contentScale = ContentScale.Crop
                 )
             }
         }
+
+        Log.d("식물 정보", "${plantViewModel.crawlPlantName} / ${plantViewModel.crawlPlantDescription}")
 
         ModalBottomSheetLayout(
             sheetState = sheetState,
@@ -160,32 +191,22 @@ fun SearchPlant(plantViewModel: PlantViewModel = hiltViewModel()) {
 
                 Column(
                     modifier = Modifier
-//                            .align(Alignment.CenterHorizontally) // 하단 중앙 정렬
                         .padding(16.dp)
-//                                .background(Color.Black.copy(alpha = 0.7f))
                 ) {
                     Text(
+//                        text = "넓은잎외잎쑥",
                         text = plantViewModel.crawlPlantName.value,
                         style = MaterialTheme.typography.titleLarge
                     )
                     Text(
+//                        text = "쌍떡잎식물 초롱꽃목 국화과의 여러해살이풀. 산지에서 자란다. 줄기는 곧게 서고, 상부에서 짧은 가지로 갈 라지며 땅속줄기가 사방으로 뻗으면서 퍼져 나가고, 처음에는 식물체가 거미줄 같은 털로 덮인다. 높이 50∼100cm이다. 뿌리잎은 꽃이 필 때쯤 되면 사라지고 줄기잎은 어긋나며 잎자루에 날개가 있다.",
                         text = plantViewModel.crawlPlantDescription.value,
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
-
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .fillMaxHeight()
-//                        .background(Color.White.copy(alpha = 0.7f))
-//                        .padding(16.dp)
-//                )
-
                     }
-//                }
-
         ){}
 
     }
+
 }
