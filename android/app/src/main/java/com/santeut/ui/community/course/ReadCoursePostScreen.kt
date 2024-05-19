cda678ef96b48c90520155914e03e747483b6564
양윤모 <yym4481@naver.com>
2024-05-20 03:42:50 +0900

    Feat: 코스 공유 게시판 쓰기 기능 구현 (partyUserId저장)

---
+++ android/app/src/main/java/com/santeut/ui/community/course/ReadCoursePostScreen.kt
@@ -0,0 +1,168 @@
+package com.santeut.ui.community.course
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.santeut.data.model.response.PostResponse
+import com.santeut.ui.community.CommonViewModel
+import com.santeut.ui.community.PostViewModel
+import com.santeut.ui.community.common.CommentScreen
+import com.santeut.ui.community.tips.formatTime
+import com.santeut.ui.navigation.top.DefaultTopBar
+import com.santeut.ui.navigation.top.TopBar
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ReadPostScreen(
+    postId: Int,
+    postType: Char,
+    postViewModel: PostViewModel,
+    commonViewModel: CommonViewModel,
+    navController: NavController
+) {
+    val focusManager = LocalFocusManager.current
+    var comment by remember { mutableStateOf("") }
+    val post by postViewModel.post.observeAsState()
+    val commentList = post?.commentList
+
+    LaunchedEffect(key1 = postId) {
+        postViewModel.readPost(postId, postType)
+    }
+
+    Scaffold(
+
+        topBar = {
+            DefaultTopBar(navController, "커뮤니티")
+        },
+
+        bottomBar = {
+            if (post != null) {
+                com.santeut.ui.guild.CommentSection(postId, postType)
+            }
+        },
+        content = { innerPadding ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Column {
+                    HeaderSection(post)
+                    ContentSection(post)
+                    commentList?.let{
+                        CommentScreen(commentList)
+                    }
+                }
+            }
+        }
+    )
+}
+
+
+@Composable
+fun HeaderSection(post: PostResponse?) {
+    post?.let {
+        val category = when (post.postType) {
+            'T' -> "등산Tip"   // 'T'일 때 "등산Tip"
+            'C' -> "등산 코스" // 'C'일 때 "등산 코스"
+            else -> "기타"     // 그 외의 경우 "기타"
+        }
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = category, style = MaterialTheme.typography.bodyMedium)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text(text = post.postTitle, style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Outlined.FavoriteBorder,
+                    contentDescription = "Like",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text(text = post.userNickname, style = MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = formatTime(post.createdAt), style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentSection(post: PostResponse?) {
+    post?.let {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(200.dp),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Text(text = post.postContent, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
+
+@Composable
+fun CommentSection(
+    postId: Int,
+    postType: Char,
+    comment: String,
+    onCommentChange: (String) -> Unit,
+    onSend: () -> Unit,
+    commonViewModel: CommonViewModel,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+//        CommentScreen(postId, postType, commonViewModel)
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        fun onSend() {
+            commonViewModel.createComment(postId, postType, comment)
+        }
+        TextField(
+            value = comment,
+            onValueChange = onCommentChange,
+            placeholder = { Text("내용") },
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Transparent),
+//            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { onSend() })
+        )
+        IconButton(onClick = { onSend() }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.Send,
+                contentDescription = "Send"
+            )
+        }
+    }
+}
