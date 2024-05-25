a2879815eba9077062c37ed6ca22ce41a1416416
양윤모 <yym4481@naver.com>
2024-05-25 20:31:31 +0900

    Revert "Merge branch 'community' into 'master'"
    This reverts merge request !127

--- community/src/main/java/com/santeut/community/common/exception/FeignClientException.java
+++
@@ -1,8 +0,0 @@
-package com.santeut.community.common.exception;
-
-//필요한 에러 이렇게 만들어서 쓰세요
-public class FeignClientException extends RuntimeException{
-    public FeignClientException(String message) {
-        super(message);
-    }
-}
